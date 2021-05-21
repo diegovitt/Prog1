a290fc023f0c8cbfe5afa2d5acae77df96b8cef3
diegovitt <diego_vitale@hotmail.com>
2021-05-21 12:57:02 -0300

    Prueba2

---
+++ EjercicioExcepciones/EjercicioExcepciones/EjemploExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    public class EjemploExceptions
+    {
+        public static void ThrowCustomException()
+        {
+            throw new CustomException();
+        }
+    }
+}
