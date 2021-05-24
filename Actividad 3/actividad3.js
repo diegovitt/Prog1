682a8e82f8ad70800de0c81010b48465daa1e86f
diegovitt <diego_vitale@hotmail.com>
2021-05-24 17:29:41 -0300

    agregadosEventos

---
+++ Actividad 3/actividad3.js
@@ -0,0 +1,12 @@
+function hacer_click() {
+    alert("gracias por pulsarme");
+}
+
+function pasar_mouse() {
+    alert("gracias por pasar sobre mi");
+
+}
+
+function sacar_mouse() {
+    alert("¡¡¡Adios, alumno de ICOP!!!")
+}
